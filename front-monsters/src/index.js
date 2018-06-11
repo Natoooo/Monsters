9ebcfe4a4a3d9f19cfc019cb77a9e654733c1792
Natalia PELOZUELO <pelozuelonatalia@gmail.com>
2018-06-11 14:34:35 +0200

    Beginning Front - basic react

---
+++ front-monsters/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Mysn from './mysn';
+
+ReactDOM.render(<Mysn />, document.getElementById('container'));
---
+++ front-monsters/src/mysn.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Mysn extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>MONSTERS</h1>
+        </header>
+        <p>
+        My Social Network
+        </p>
+      </div>
+    );
+  }
+}
