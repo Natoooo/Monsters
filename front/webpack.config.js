bd9cf1a3441433f5305d18ae7b55f91663c3c7a0
Pelozuelo <pelozuelonatalia@gmail.com>
2018-09-13 11:21:07 +0200

    my webpack config done

---
+++ front/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './dist'
+  },
+  devtool: 'source-map',
+  stats: {
+    errorDetails: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
