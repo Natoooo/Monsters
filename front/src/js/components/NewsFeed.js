9af625cef45114e550f95746a782454f6ab2be9d
Pelozuelo <pelozuelonatalia@gmail.com>
2018-09-27 11:50:51 +0200

    add link to profile from the name in the post

--- front/src/js/components/NewsFeed.js
+++ front/src/js/components/NewsFeed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux"
 import { fetchPosts, removePost } from "../actions/postActions"
+import { fetchUser } from "../actions/userActions"
 import Loading from "./Loading"
 import ErrorMessage from "./Error"
 
@@ -9,6 +10,7 @@ class NewsFeed extends Component {
     super(props)
 
     this.removePost = this.removePost.bind(this)
+    this.changeProfile = this.changeProfile.bind(this)
   }
 
   componentDidMount() {
@@ -19,6 +21,11 @@ class NewsFeed extends Component {
   removePost(postId) {
     console.log("FETCH_DELETE_POST", this.props.removePost)
     this.props.removePost(postId)
+  }
+
+  changeProfile(userId) {
+    console.log("OPEN_PROFILE", userId)
+    this.props.fetchUser(userId)
   }
 
   render() {
@@ -32,8 +39,11 @@ class NewsFeed extends Component {
               <h4 className="p-2 font-weight-bold">{post.title}<span className="col-2" onClick={() => {this.removePost(post.id)}} className="mts-delete-post">X</span></h4>
             </div>
           </div>
+          <div className="form-group mb-0">
+            <div className="container border-top border-bottom mts-profile-click" onClick={()=> {this.changeProfile(post.user.id)}}><em>By {post.user.name}</em></div>
+          </div>
           <div className="form-group">
-            <div className="container border-top border-bottom"><em>By {post.user.name}</em></div>
+            <div className="container border-bottom"><em>{post.posted_at.substr(0, 10)}</em></div>
           </div>
           <div className="form-group">
             <div className="container">{post.content}</div>
@@ -42,7 +52,7 @@ class NewsFeed extends Component {
             {post.image != 0 ? <img className="mw-100" src={post.image}/> : ""}
           </div>
           <div className="form-group p-2">
-            <span>{post.posted_at.substr(0, 10)}</span>
+            <span></span>
           </div>
         </div>
       )
@@ -65,7 +75,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return ({
     fetchPosts: () => { dispatch(fetchPosts()) },
-    removePost: (postId) => { dispatch(removePost(postId)) }
+    removePost: (postId) => { dispatch(removePost(postId)) },
+    fetchUser: (userId) => { dispatch(fetchUser(userId)) }
   })
 }
 
